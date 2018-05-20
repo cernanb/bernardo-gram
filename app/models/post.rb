a0ef2cabb7a82dd24bcd38db2fc5661d4ecd154a
Cernan Bernardo <cernanb@gmail.com>
2018-05-19 21:27:04 -0700

    Add assoc btw users and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :image, :caption, presence: true
   has_attached_file :image, styles: {:medium => '640x'}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  belongs_to :user
 end
