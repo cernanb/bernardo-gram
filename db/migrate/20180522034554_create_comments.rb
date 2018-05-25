a7e7845e8fde8dcbb3c7ed323a4d22a8aacbd025
Cernan Bernardo <cernanb@gmail.com>
2018-05-24 22:35:33 -0700

    Update comment migration

--- db/migrate/20180522034554_create_comments.rb
+++ db/migrate/20180522034554_create_comments.rb
@@ -1,11 +1,13 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.references :user, foreign_key: true
-      t.references :post, foreign_key: true
+      t.references :user, index: true
+      t.references :post, index: true
       t.text :content
 
-      t.timestamps
+      t.timestamps null: false
     end
+    add_foreign_key :comments, :users
+    add_foreign_key :comments, :posts
   end
 end
