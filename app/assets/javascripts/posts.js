9a0bdd30c2dc887e8dabcca8e86c7e4a408261f9
Cernan Bernardo <cernanb@gmail.com>
2018-06-03 23:46:51 -0700

    Add comment

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,3 +1,4 @@
+// change the image when a user posting a pic
 var loadFile = event => {
   var output = document.getElementById('image-preview')
   output.src = URL.createObjectURL(event.target.files[0])
